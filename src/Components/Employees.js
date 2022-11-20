4ef66da5b1e574f59ae0392a75f5cac99405cf67
Tarakesh-K <tister9@gmail.com>
2022-11-20 13:37:41 +0530

    first commit

---
+++ src/Components/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+  {
+    id: "1",
+    Name: "Manoj Deshwal",
+    Age: "30"
+  }, {
+    id: "2",
+    Name: "Virat Kohli",
+    Age: "32"
+  }, {
+    id: "3",
+    Name: "Rohit Sharma",
+    Age: "33"
+  }
+]
+
+export default Employees;
